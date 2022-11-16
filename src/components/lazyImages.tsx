02a0037bc9412cb6596a74b034d7fd8e1847e54e
Juhyeon <gomi.dev1755@gmail.com>
2022-11-17 06:34:46 +0900

    [fix] bug fix

--- src/components/lazyImages.tsx
+++ src/components/lazyImages.tsx
@@ -4,14 +4,18 @@ const videoStyle: CSSProperties = {
   width: '100%',
 };
 
-export default function LazyImages({ test }:{test:string}) {
-  return test && ((test.split('.')[1] === 'mp4') || (test.split('.')[1] === 'webm'))
-    ? (
-      <video style={videoStyle} autoPlay loop muted>
-        <source src={test} />
-      </video>
-    )
-    : (
-      <img style={videoStyle} src={test} alt="test" />
-    );
+export default function LazyImages({ test }: { test: string }) {
+  if (test) {
+    return ((test.split('.')[1] === 'mp4') || (test.split('.')[1] === 'webm'))
+      ? (
+        <video style={videoStyle} autoPlay loop muted>
+          <source src={test} />
+        </video>
+      )
+      : (
+        <img style={videoStyle} src={test} alt="test" />
+      );
+  }
+
+  return null;
 }
